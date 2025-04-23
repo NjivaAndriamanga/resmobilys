2e4ddab55fffcb8b7337d940a99ed1ab46b194fe
NivaAndriamanga <andriamanga.bioinfo@gmail.com>
2025-04-23 14:28:37 +0200

    rgi2gff correction

--- bin/GFF_parsing/rgi2gff.sh
+++ bin/GFF_parsing/rgi2gff.sh
@@ -1,19 +1,15 @@
-BEGIN { 
-	OFS="\t"
-	print "##gff-version 3"
+BEGIN {
+    OFS = "\t"
+    print "##gff-version 3"
 }
 
 NR > 1 {
-    # Match sequence ID pattern: chromosome/plasmid/contig/reads/etc
-    if (match($2, /[a-zA-Z]+[0-9]+/, a)) {
-    	seqid = substr($2,RSTART, RLENGTH)
+    if (match($2, /[a-zA-Z]+[0-9]+/)) {
+        seqid = substr($2, RSTART, RLENGTH)
     } else {
-    	seqid = $2
+        seqid = $2
     }
-    seqid = a[0]
 
-    # Build the attribute field with ID, Note, and DRUG
     attr = "ID=" $6 ";Note=" $9 ";DRUG=" $7
-
     print seqid, "RGI", "CDS", $3, $4, ".", $5, "0", attr
 }
