29663aeffcf0278d3a7764d892abc3912f02f797
NivaAndriamanga <andriamanga.bioinfo@gmail.com>
2025-06-16 15:27:59 +0200

    tcomp2GFF correction

--- bin/GFF_parsing/tcomp2gff.sh
+++ bin/GFF_parsing/tcomp2gff.sh
@@ -8,12 +8,16 @@ BEGIN {
     split($2, parts, "=")
     seqid = parts[1]
 }
-/^[AB] transposon/ {
+/A transposon/ {
     split($3, coords, /\.\./)
     start = coords[1]
-    end = coords[2]
     strand = $4
     name = $6
+}
+
+/B transposon/ {
+    split($3, coords, /\.\./)
+    end = coords[2]
     print seqid, "tcomp", "transposable_element", start, end, ".", strand, ".", "ID=" id ";Name=" name
     id++
 }
