c4b9aa477addefdd29be376cf545863becf3fddd
NivaAndriamanga <andriamanga.bioinfo@gmail.com>
2025-10-29 11:31:54 +0100

    blast to gff correction

--- bin/GFF_parsing/blast2gff.sh
+++ bin/GFF_parsing/blast2gff.sh
@@ -1,8 +1,8 @@
 BEGIN {FS=OFS="\t"}
 {
-  strand = ($9 < $10) ? "+" : "-";
-  start = ($9 < $10) ? $9 : $10;
-  end = ($9 < $10) ? $10 : $9;
+  strand = ($8 < $9) ? "+" : "-";
+  start = ($8 < $9) ? $8 : $9;
+  end = ($8 < $9) ? $9 : 10;
   print $3, "BLAST", "match", start, end, $13, strand, ".", \
-        "ID=" $2 ";Identity=" $4 ";Evalue=" $12 ";Length=" $5 ";Description=" $1;
+        "sample="pre";ID=" $2 ";Identity=" $4 ";Evalue=" $12 ";Length=" $5 ";Description=" $1;
 }
